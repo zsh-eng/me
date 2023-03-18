c6975491c371ff7b85759e53e6344a7154e55217
sheng <49238630+zsh-eng@users.noreply.github.com>
2023-03-19 01:26:14 +0800

    feat: Add landing page skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        pattern: "radial-gradient(rgba(255, 255, 255, 0.1) 9%, transparent 9%)",
+        // pattern:
+        //   "linear-gradient(to right, transparent 9%, rgba(255, 255, 255, 0.1) 9%), linear-gradient(to bottom, transparent 9%, rgba(255, 255, 255, 0.1) 9%)",
+      },
+      backgroundSize: {
+        "pattern-size": "12vmin 12vmin",
+      },
+    },
   },
   plugins: [],
 };
