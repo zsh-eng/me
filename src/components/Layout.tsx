f86dde0a9a8bdc57149d1f5aea03a09eb3efcc35
sheng <chengzhisheng2001@gmail.com>
2023-08-09 10:58:02 +0800

    Clean up layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import { type PropsWithChildren } from "react";
 
 type LayoutProps = {
   activeIndex: number;
 };
 
-const Layout = ({ children, activeIndex }: PropsWithChildren<LayoutProps>) => {
+const Layout = ({ children }: PropsWithChildren<LayoutProps>) => {
   return (
     <main
       className="relative flex min-h-screen flex-col items-start justify-center overflow-hidden bg-base-100 sm:items-center"
